ad16138339bc69e03fee23f20422d370240ee829
sum <scausum@163.com>
2016-05-20 16:45:45 +0800

    在OkHttpClientHolder中添加addNetworkInterceptor方法方便全局配置

--- okhttphelper/src/main/java/com/fubaisum/okhttphelper/OkHttpClientHolder.java
+++ okhttphelper/src/main/java/com/fubaisum/okhttphelper/OkHttpClientHolder.java
@@ -1,6 +1,9 @@
 package com.fubaisum.okhttphelper;
 
 
+import java.util.List;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 
 /**
@@ -16,6 +19,18 @@ public class OkHttpClientHolder {
 
     public static OkHttpClient getOkHttpClient() {
         return okHttpClient;
+    }
+
+    public static void addNetworkInterceptor(Interceptor networkInterceptor) {
+        okHttpClient = okHttpClient.newBuilder().addNetworkInterceptor(networkInterceptor).build();
+    }
+
+    public static void addNetworkInterceptors(List<Interceptor> networkInterceptors) {
+        OkHttpClient.Builder builder = okHttpClient.newBuilder();
+        for (Interceptor interceptor : networkInterceptors) {
+            builder.addNetworkInterceptor(interceptor);
+        }
+        okHttpClient = builder.build();
     }
 
 }
